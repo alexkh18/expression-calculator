eb35ea248b0072336e139689f8197c9adc1e07f7
Aliaksandr Khudnitski <alex2khudnitski@gmail.com>
2020-03-24 23:21:30 +0300

    feat: add new solution

--- src/index.js
+++ src/index.js
@@ -3,82 +3,86 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
-    expr = expr.replace(/\s/g, '').replace(/(\*|\/|\+|\-)/g, ' $& ');
-   let leftBrackets = 0;
-   let rightBrackets = 0;
-
+function expressionCalculator(expr) {    
+    function getSubExprFromPriorityBrackets() {
+        const endIndex = expr.search(/\)/);
+        let startIndex = expr.split(/\)/)[0];
+            startIndex = endIndex - startIndex.split('').reverse().join('').search(/\(/);
+        return expr.slice(startIndex, endIndex);
+    }
+    
+    let openingBrackets = 0;
+    let closingBrackets = 0;
     for (let i = 0; i < expr.length; i++) {
-        if (expr[i] === '(') {
-            leftBrackets += 1;
-        }
-        if (expr[i] == ')') {
-            rightBrackets += 1;
+        switch (true) {
+            case (expr[i] === '('): openingBrackets += 1;
+                break;
+            case (expr[i] === ')'): closingBrackets += 1;
+                break;
         }
     }
 
-    if (leftBrackets !== rightBrackets) {
+    let priorityExpr = expr;
+    if(openingBrackets !== closingBrackets){
         throw new Error("ExpressionError: Brackets must be paired");
+    } else if (openingBrackets > 0) {
+        priorityExpr = getSubExprFromPriorityBrackets();
+        const calculatedExpr = calculate(priorityExpr);
+        expr = expr.replace(`(${priorityExpr})`, calculatedExpr)
+        return expressionCalculator(expr)
     }
-
-    let brackets;
-    while (leftBrackets > 0) {
-        if ((brackets = expr.match(/(\([0-9\+\/\*\-. ]+\))/g)) !== null) {
-            for (let i = 0; i < brackets.length; i++) {
-                let str = brackets[i].replace('(', '').replace(')', '');
-                expr = expr.replace(brackets[i], calculate(str));
+    const result = calculate(priorityExpr);
+    
+    function calculate(currExprPart) {
+        function mathOperations(a, b, operationType){
+            switch (operationType){
+                    case '+': return Number(a) + Number(b);
+                    case '-': return Number(a) - Number(b);
+                    case '/': return Number(a) / Number(b);
+                    case '*': return Number(a) * Number(b);
             }
         }
-        leftBrackets -= 1;
-    }
-    return calculate(expr);
-}
 
-function calculate(expr) {
+        function deleteEmptyArrCells(arr, i){
+            arr.splice(i - 1, 1);
+            arr.splice(i, 1);
+        };
 
-    function mathOperations(a, b, operationType){
-        switch (operationType){
-                case '+': return Number(a) + Number(b);
-                case '-': return Number(a) - Number(b);
-                case '/': return Number(a) / Number(b);
-                case '*': return Number(a) * Number(b);
+        if(currExprPart.trim().includes(' ') === false) {
+            currExprPart = currExprPart.replace(/(\*|\/|\+|\-)/g, ' $& ');
         }
-    }
-
-    function deleteEmptyArrCells(arr, i){
-        arr.splice(i - 1, 1);
-        arr.splice(i, 1);
-    };
-
-    let currExprPart = expr.split(' ');
-    for (let i = 0; i < currExprPart.length; i++) {
-        if (currExprPart[i] === "*") {
-            currExprPart[i] = mathOperations( currExprPart[i - 1], currExprPart[i + 1], '*');
-            deleteEmptyArrCells(currExprPart, i);
-            i = i - 1;
-        }
-        if (currExprPart[i] === "/") {
-            if (currExprPart[i + 1] === '0') {
-                throw new TypeError('TypeError: Division by zero.');
+        currExprPart = currExprPart.split(' ');
+        for (let i = 0; i < currExprPart.length; i++) {
+            if (currExprPart[i] === "*") {
+                currExprPart[i] = mathOperations( currExprPart[i - 1], currExprPart[i + 1], '*');
+                deleteEmptyArrCells(currExprPart, i);
+                i = i - 1;
             }
-            currExprPart[i] = mathOperations( currExprPart[i - 1], currExprPart[i + 1], '/');
-            deleteEmptyArrCells(currExprPart, i);
-            i = i - 1;
+            if (currExprPart[i] === "/") {
+                if (currExprPart[i + 1] === '0') {
+                    throw new TypeError('TypeError: Division by zero.');
+                }
+                currExprPart[i] = mathOperations( currExprPart[i - 1], currExprPart[i + 1], '/');
+                deleteEmptyArrCells(currExprPart, i);
+                i = i - 1;
+            }
         }
+        for (let i = 0; i < currExprPart.length; i++) {
+            if (currExprPart[i] === "+") {
+                currExprPart[i] = mathOperations( currExprPart[i - 1], currExprPart[i + 1], '+');
+                deleteEmptyArrCells(currExprPart, i);
+                i -= 1;
+            }
+            if (currExprPart[i] === "-") {
+                currExprPart[i] = mathOperations( currExprPart[i - 1], currExprPart[i + 1], '-');
+                deleteEmptyArrCells(currExprPart, i);
+                i -= 1;
+            }
+        }
+        return Number(currExprPart.join('').trim());
     }
-    for (let i = 0; i < currExprPart.length; i++) {
-        if (currExprPart[i] === "+") {
-            currExprPart[i] = mathOperations( currExprPart[i - 1], currExprPart[i + 1], '+');
-            deleteEmptyArrCells(currExprPart, i);
-            i -= 1;
-        }
-        if (currExprPart[i] === "-") {
-            currExprPart[i] = mathOperations( currExprPart[i - 1], currExprPart[i + 1], '-');
-            deleteEmptyArrCells(currExprPart, i);
-            i -= 1;
-        }
-    }
-    return Number(currExprPart[0]);
+
+return result
 }
 
 module.exports = {
